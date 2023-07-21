ec16fd7fdf74a104aa9a50c8f7f73d8f1d3e4eb2
Young-Gaius <youngehu8@gmail.com>
2023-07-21 23:10:05 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './index.css';
+const tasks = [
+    { description: 'Task 1', completed: false, index: 1 },
+    { description: 'Task 2', completed: true, index: 2 },
+    { description: 'Task 3', completed: false, index: 3 },
+    // Add more tasks here if needed
+  ];
+  
+  function renderTasks() {
+    const todoList = document.getElementById('todo-list');
+    todoList.innerHTML = ''; // Clear the existing content
+  
+    tasks
+      .sort((a, b) => a.index - b.index) // Sort tasks based on the index
+      .forEach((task) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = task.description;
+        listItem.classList.add(task.completed ? 'completed' : 'pending');
+        todoList.appendChild(listItem);
+      });
+  }
+  
+  document.addEventListener('DOMContentLoaded', renderTasks);
+  
